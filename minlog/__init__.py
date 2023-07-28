bf896b98e78e9971c4dd889d4db1a5fabb95112c
redthing1 <redthing1@alt.icu>
2023-07-27 19:24:28 -0700

    add support for log sources

--- minlog/__init__.py
+++ minlog/__init__.py
@@ -31,9 +31,12 @@ class Verbosity(Enum):
 
 
 class Logger(object):
-    def __init__(self, verbosity: Verbosity = Verbosity.INFO):
+    def __init__(self, verbosity: Verbosity = Verbosity.INFO, log_source: str = None):
         # maximum verbosity of messages to print
         self.verbosity = verbosity
+
+        # log source tag
+        self.log_source = log_source
 
         # default to rich
         self.backend = Backend.RICH
@@ -50,19 +53,9 @@ class Logger(object):
             # initialize colorama
             colorama.init()
 
-    def short_verbosity(self, message_verbosity: Verbosity):
-        if message_verbosity == Verbosity.DEBUG:
-            return "dbg"
-        elif message_verbosity == Verbosity.TRACE:
-            return "trc"
-        elif message_verbosity == Verbosity.INFO:
-            return "inf"
-        elif message_verbosity == Verbosity.WARN:
-            return "wrn"
-        elif message_verbosity == Verbosity.ERROR:
-            return "err"
-        elif message_verbosity == Verbosity.CRITICAL:
-            return "crt"
+    def logger_for(self, log_source: str):
+        # create a clone of this logger, but with a different log source
+        return Logger(self.verbosity, log_source=log_source)
 
     def log(self, message: str, message_verbosity: Verbosity):
         # if the message verbosity is within the configured maximum verbosity
@@ -70,25 +63,37 @@ class Logger(object):
             return
 
         # then print the message
-        meta_str = f"{self.short_verbosity(message_verbosity)}"
+        meta_str = f"[{self.short_verbosity(message_verbosity)}]"
         message_str = message
+        source_str = ""
+        if self.log_source is not None:
+            source_str = f"[{self.log_source}]"
 
         if self.backend == Backend.RICH:
             # mprint(message)
-            meta_style_rich = self.get_style_rich(message_verbosity)
-            self.rich_console.print(f"\[{meta_str}]", style=meta_style_rich, end="")
+            meta_str_escaped = meta_str.replace("[", "\[")
             message_str_escaped = message_str.replace("[", "\[")
+            source_str_escaped = source_str.replace("[", "\[")
+
+            meta_style_rich = self.get_style_rich(message_verbosity)
+            self.rich_console.print(meta_str_escaped, style=meta_style_rich, end="")
+            self.rich_console.print(" ", end="")
+            self.rich_console.print(
+                f"{source_str_escaped}", style="bright_black", end=""
+            )
             self.rich_console.print(f" {message_str_escaped}")
         elif self.backend == Backend.COLORAMA:
             meta_bg = Back.BLACK
             meta_fg = self.get_fg_color_colorama(message_verbosity)
+            source_fg = Fore.LIGHTBLACK_EX
             message_bg = Back.RESET
             message_fg = Fore.RESET
 
-            meta_frm = f"{meta_bg}{meta_fg}[{meta_str}]{Style.RESET_ALL}"
+            meta_frm = f"{meta_bg}{meta_fg}{meta_str}{Style.RESET_ALL}"
+            source_frm = f"{source_fg}{source_str}{Style.RESET_ALL}"
             message_frm = f"{message_bg}{message_fg}{message_str}{Style.RESET_ALL}"
 
-            print(f"{meta_frm} {message_frm}")
+            print(f"{meta_frm} {source_frm} {message_frm}")
 
     def debug(self, message: str):
         self.log(message, Verbosity.DEBUG)
--- minlog/_test.py
+++ minlog/_test.py
@@ -1,19 +1,29 @@
 from . import logger, Verbosity, Backend
 
 
-def all_prints():
-    logger.log("hello", Verbosity.DEBUG)
-    logger.log("hello", Verbosity.TRACE)
-    logger.log("hello", Verbosity.INFO)
-    logger.log("hello", Verbosity.WARN)
-    logger.log("hello", Verbosity.ERROR)
-    logger.log("hello", Verbosity.CRITICAL)
+def log_all_levels(my_logger):
+    my_logger.log("hello", Verbosity.DEBUG)
+    my_logger.log("hello", Verbosity.TRACE)
+    my_logger.log("hello", Verbosity.INFO)
+    my_logger.log("hello", Verbosity.WARN)
+    my_logger.log("hello", Verbosity.ERROR)
+    my_logger.log("hello", Verbosity.CRITICAL)
 
 
 if __name__ == "__main__":
     print("=" * 10, "RICH", "=" * 10)
     logger.backend = Backend.RICH
-    all_prints()
+    log_all_levels(logger)
     print("=" * 10, "COLORAMA", "=" * 10)
     logger.backend = Backend.COLORAMA
-    all_prints()
+    log_all_levels(logger)
+
+    # test with a log source
+    print("=" * 10, "LOG SOURCE (RICH)", "=" * 10)
+    apple_logger = logger.logger_for("apple")
+    log_all_levels(apple_logger)
+
+    print("=" * 10, "LOG SOURCE (COLORAMA)", "=" * 10)
+    banana_logger = logger.logger_for("banana")
+    banana_logger.backend = Backend.COLORAMA
+    log_all_levels(banana_logger)
